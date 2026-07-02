53cfb7061cef1d40105a92e4454fbd6c855701d2
peterk <peterkudelas@gmail.com>
2026-07-02 12:21:05 +0200

    batmaaan

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@comark/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/seo',
+    'nuxt-auth-utils'
+  ]
+})
